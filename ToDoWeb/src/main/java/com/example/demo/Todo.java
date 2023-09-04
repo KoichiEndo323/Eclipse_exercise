416912f5794f7b6635ab1b663d00506c78c7c68c
KoichiEndo323 <waddledee323@gmail.com>
2023-09-04 23:50:09 +0900

    [add]todo

--- ToDoWeb/src/main/java/com/example/demo/Todo.java
+++ ToDoWeb/src/main/java/com/example/demo/Todo.java
@@ -1,5 +1,17 @@
 package com.example.demo;
 
+
 public class Todo {
+	
+	public static void name() {
+		
+	}
+	
+	
+	
+	
+	public static String fullName(String firstName, String LastName) {
+		return firstName + " " + LastName;
+	}
 
 }
